a7619ff92f6c224df389c1e7664a6928747b5024
OmChauhan07 <odchauhan0702@gmail.com>
2026-05-08 02:24:18 +0530

    Deployment Modifications

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: 'https://github.com/OmChauhan07/om_portfolio.git',
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
